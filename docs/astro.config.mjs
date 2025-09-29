df472dcca993afc5ff269273c6c8fda8db1abfc6
Bennett Moore <120052232+bambam955@users.noreply.github.com>
2025-09-29 04:30:37 -0400

    docs: astro site (#22)
    Co-authored-by: Addison <addison.emig@mrs-electronics.com>
    Co-authored-by: Addison Emig <addison@kwila.cloud>

---
+++ docs/astro.config.mjs
@@ -0,0 +1,36 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://qt.mrs-electronics.dev',
+	base: '/',
+	outDir: './dist',
+	publicDir: './public',
+	integrations: [
+		starlight({
+			title: 'MRS Qt SDK',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/mrs-electronics-inc/mrs-sdk-qt' }],
+			sidebar: [
+				{
+					label: 'Guides',
+					autogenerate: { directory: 'guides' },
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+			head: [
+				{
+					tag: 'link',
+					attrs: {
+						rel: 'manifest',
+						href: '/site.webmanifest'
+					}
+				}
+			]
+		}),
+	],
+});
